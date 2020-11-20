62209d71c5a8284bd63cf0b4c3778fbd86e5865d
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-11-20 17:04:23 +0100

    Add main activity

--- app/src/main/java/com/example/sym_labo03/MainActivity.java
+++ app/src/main/java/com/example/sym_labo03/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sym_labo03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button barcodeBtn = findViewById(R.id.main_barcode_btn);
+        Button ibeaconBtn = findViewById(R.id.main_ibeacon_btn);
+        Button nfcBtn = findViewById(R.id.main_nfc_btn);
+
+/*        barcodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.class, )
+                startActivity(intent);
+            }
+        });*/
+
+/*        ibeaconBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.class, )
+                startActivity(intent);
+            }
+        });*/
+
+/*        nfcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.class, )
+                startActivity(intent);
+            }
+        });*/
     }
 }
