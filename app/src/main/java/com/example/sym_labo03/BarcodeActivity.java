da024ed2104fc187fe743691a3daf9de13f7e50e
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-11-20 18:29:35 +0100

    add scanner

--- app/src/main/java/com/example/sym_labo03/BarcodeActivity.java
+++ app/src/main/java/com/example/sym_labo03/BarcodeActivity.java
@@ -2,13 +2,87 @@ package com.example.sym_labo03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class BarcodeActivity extends AppCompatActivity {
+    private String lastText;
+
+    private DecoratedBarcodeView barcodeView;
+
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() == null || result.getText().equals(lastText)) {
+                // Prevent duplicate scans
+                return;
+            }
+
+            lastText = result.getText();
+
+            // Set the image
+            ImageView preview = findViewById(R.id.barcode_preview);
+            preview.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+
+            // Set the text
+            TextView decodedText = findViewById(R.id.barcode_decodedtext_tv);
+            decodedText.setText(result.getText());
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+
+        barcodeView = findViewById(R.id.barcode_scanner);
+
+        Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39);
+
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        barcodeView.initializeFromIntent(getIntent());
+        barcodeView.decodeContinuous(callback);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 }
+
