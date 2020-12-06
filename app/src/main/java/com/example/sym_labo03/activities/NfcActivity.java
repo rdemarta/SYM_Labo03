f02c4fcc0f9f0eb2a1044b7e66c05ca2575909fe
Robin Demarta <r.demarta@outlook.com>
2020-12-06 16:01:20 +0100

    Extract reading process to NfcReader class

--- app/src/main/java/com/example/sym_labo03/activities/NfcActivity.java
+++ app/src/main/java/com/example/sym_labo03/activities/NfcActivity.java
@@ -8,11 +8,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -23,12 +19,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sym_labo03.NfcReader;
 import com.example.sym_labo03.R;
 
-import java.io.UnsupportedEncodingException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class NfcActivity extends AppCompatActivity {
 
@@ -38,7 +33,6 @@ public class NfcActivity extends AppCompatActivity {
     private boolean nfcOk = false;
 
     private static final String TAG = "NFC_ACTIVITY";
-    private static final String MIME_TEXT_PLAIN = "text/plain";
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "secret";
     private static final String[] NFC_PWD = new String[]{"test", "é è ê ë ē", "♤ ♡ ♢ ♧"};
@@ -53,6 +47,7 @@ public class NfcActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             NfcActivity activity = mActivity.get();
             if (activity != null) {
+                // Give NFC results to activity and proceed to verification
                 activity.verifyNfcValues(msg.getData().getStringArrayList("results"));
             }
         }
@@ -111,64 +106,7 @@ public class NfcActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
-
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-                Thread readNfc = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Ndef ndef = Ndef.get(tag);
-
-                        if (ndef != null) {
-                            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-                            NdefRecord[] records = ndefMessage.getRecords();
-                            ArrayList<String> results = new ArrayList<>();
-
-                            for (NdefRecord ndefRecord : records) { // Read every line
-                                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                                    try {
-                                        String result = readText(ndefRecord);
-                                        results.add(result);
-                                        Log.d(TAG, result);
-                                    } catch (UnsupportedEncodingException e) {
-                                        Log.e(TAG, "Unsupported Encoding", e);
-                                    }
-                                }
-                            }
-
-                            Message msg = handler.obtainMessage();
-                            Bundle bundle = new Bundle();
-                            bundle.putStringArrayList("results", results);
-                            msg.setData(bundle);
-                            handler.sendMessage(msg);
-                        }
-                    }
-                });
-                readNfc.start();
-
-            } else {
-                Log.d(TAG, "Wrong mime type: " + type);
-            }
-        } /*else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-
-            // In case we would still use the Tech Discovered Intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
-
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    //new NdefReaderTask().execute(tag); TODO ?
-                    break;
-                }
-            }
-        }*/
+        new NfcReader(intent, handler);
     }
 
     /**
@@ -225,30 +163,4 @@ public class NfcActivity extends AppCompatActivity {
             mNfcAdapter.disableForegroundDispatch(this);
     }
 
-    // Source: https://code.tutsplus.com/tutorials/reading-nfc-tags-with-android--mobile-17278
-    private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-        byte[] payload = record.getPayload();
-
-        // Get the Text Encoding
-        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-        // Get the Language Code
-        int languageCodeLength = payload[0] & 51;
-
-        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-        // e.g. "en"
-
-        // Get the Text
-        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-    }
 }
